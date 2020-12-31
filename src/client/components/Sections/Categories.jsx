0697df42cc5a324cad576d4b9000d0865ba44cdb
Santiago <santiagojaviermartinez@gmail.com>
2020-12-31 05:27:25 -0300

    las categorias se mapean como cards en Home

---
+++ src/client/components/Sections/Categories.jsx
@@ -0,0 +1,20 @@
+import guia from '../../images/guiatesis.jpg'
+import nosotros from '../../images/sobrenosotros.jpg'
+
+const categories = [
+    {
+        title: 'Guía de tesis',
+        description: 'Guías, artículos, plantillas y todo lo que necesitas para armar tu tesis de grado.',
+        url: '/guiadetesis',
+        image: guia,
+    },
+
+    {
+        title: 'Sobre Nosotrxs',
+        description: 'Conoce a los creadores y colaboradores que forman parte de PsiTesis. Podrás encontrar toda la información sobre los autores de los artículos.',
+        url: '/sobrenosotrxs',
+        image: nosotros ,
+    },
+]
+
+export default categories
--- src/client/components/Sections/Home.jsx
+++ src/client/components/Sections/Home.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import { Grid, Box, Divider, Hidden, CardActionArea } from '@material-ui/core/';
 import { SectionCard, SectionCardMedia, SectionCardTitle, SectionCardDescription } from '../StyledComponents/SectionCard'
-import guia from '../../images/guiatesis.jpg'
-import nosotros from '../../images/sobrenosotros.jpg'
-
-
-
+import { Link } from 'react-router-dom'
+import categories from './Categories'
 
 function Home(props) {
 
@@ -13,43 +10,32 @@ function Home(props) {
         <Box display="flex" justifyContent="center" padding="32px" height="89%" >
             <Hidden only="xs">
                 <Grid container spacing={5} >
+                    
+                    {categories.map((category)=>(
+
                     <Grid item xs={6}>
-                        <SectionCard  >
-                            <CardActionArea>
-                                <SectionCardMedia
-                                    image={guia}
-                                />
-                                <SectionCardTitle variant="h6">
-                                    Guia de tesis
+                        <Link to={category.url} style={{ textDecoration: 'none' }}>
+                            <SectionCard >
+                                <CardActionArea>
+
+                                    <SectionCardMedia
+                                        image={category.image}
+                                    />
+                                    <SectionCardTitle variant="h6">
+                                        {category.title}
                                     </SectionCardTitle>
 
-                                <Divider variant="middle" />
+                                    <Divider variant="middle" />
 
-                                <SectionCardDescription variant="body2" color="textSecondary">
-                                    Guías, artículos, plantillas y todo lo que necesitas para armar tu tesis de grado.
-                                </SectionCardDescription>
-                            </CardActionArea>
-                        </SectionCard>
+                                    <SectionCardDescription variant="body2" color="textSecondary">
+                                        {category.description}
+                                    </SectionCardDescription>
+                                </CardActionArea>
+                            </SectionCard>
+                        </Link>
                     </Grid>
+                    ))}
 
-                    <Grid item xs={6}>
-                        <SectionCard  >
-                            <CardActionArea>
-                                <SectionCardMedia
-                                    image={nosotros}
-                                />
-                                <SectionCardTitle variant="h6">
-                                    Sobre Nosotros
-                                </SectionCardTitle>
-
-                                <Divider variant="middle" />
-
-                                <SectionCardDescription variant="body2" color="textSecondary">
-                                    Conoce a los creadores y colaboradores que forman parte de PsiTesis. Podrás encontrar toda la información sobre los autores de los artículos.
-                                </SectionCardDescription>
-                            </CardActionArea>
-                        </SectionCard>
-                    </Grid>
                 </Grid>
             </Hidden>
         </Box>
