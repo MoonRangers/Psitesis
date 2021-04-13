34788126f37a34ab75611d6d592a939526e83513
SantiagoMartinez25 <santiagojaviermartinez@gmail.com>
2021-04-12 21:42:43 -0300

    arregle grid para vista Desktop

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -3,7 +3,6 @@ import { Grid, Divider, Typography  } from '@material-ui/core/';
 import { makeStyles } from "@material-ui/core/styles";//
 import { StyledLink} from '../StyledComponents/Link'
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     welcomeDescription: {
@@ -33,22 +32,31 @@ function Home(props) {
             exit={{ opacity: 0 }}
             className={styles}
         >
-            <Grid container spacing={2} >
-                <Grid item>
+            <Grid container spacing={2}  >
+                <Grid item xs={12} >
                     <Typography variant="h6">
-                            Bienvenidos
+                        Bienvenidos
                     </Typography>     
                 </Grid>
-                <Grid item>
+                <Grid item xs={12} >
                     <Typography className={classes.bajada} >
-                        En Psitesis encontraras <StyledLink to="/app/guiadetesis">ARTÍCULOS</StyledLink> escritos por <StyledLink to="/app/sobrenosotrxs">COLABORADORES</StyledLink> expertos en la construcción de tesis.<br></br>
+                        En Psitesis encontrarás <StyledLink to="/app/guiadetesis">ARTÍCULOS</StyledLink> escritos por <StyledLink to="/app/sobrenosotrxs">COLABORADORES</StyledLink> expertos en la construcción de tesis.<br></br><br></br>
                         Si seguis con dudas podés escribir en el <StyledLink to="/app/conversacion">FORO</StyledLink>, donde encontraras otros colegas que puedan ayudarte.
                     </Typography>
 
                     <Divider />
-                    
-                </Grid>    
-            </Grid>            
+                </Grid>   
+
+                <Grid container item style={{marginTop:"8px"}}>
+                    <Grid item>
+                        <Typography variant="h6">
+                            Los artículos más leidos de la página
+                        </Typography>
+                    </Grid>  
+                </Grid>
+
+            </Grid>
+
         </motion.div>
     )
 }
